333c96464aff069a0eaa29fe1e8a5fc4328669f8
Arjit Gautam <111347114+deepanshubarve@users.noreply.github.com>
2023-10-19 23:59:02 +0530

    sampl

---
+++ app/src/main/java/com/example/playvideoexo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.playvideoexo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playerView = findViewById(R.id.playerview);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+
+
+
+
+        }
+}
